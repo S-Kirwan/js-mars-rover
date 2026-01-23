ba6e5c0dd55fd70958b0598626e906de46aa4962
Sean Kirwan <seandarrakirwan@gmail.com>
2026-01-23 10:37:25 +0000

    Data classes
    Added PleateauSize and Positon classes

---
+++ src/logic/PlateauSize.js
@@ -0,0 +1,26 @@
+class PlateauSize
+{
+	#x;
+	#y;
+	constructor(x, y)
+	{
+		this.#x = x;
+		this.#y = y;
+	}
+	get x ()
+	{
+		return (this.#x);
+	}
+	get y ()
+	{
+		return (this.#y);
+	}
+	set x (x)
+	{
+		this.#x = x;
+	}
+	set y (y)
+	{
+		this.#y = y;
+	}
+}
---
+++ src/logic/Position.js
@@ -0,0 +1,36 @@
+class Position
+{
+	#x;
+	#y;
+	#orientation;
+	constructor(x, y, orientation)
+	{
+		this.#x = x;
+		this.#y = y;
+		this.#orientation = orientation;
+	}
+	get x ()
+	{
+		return (this.#x);
+	}
+	get y ()
+	{
+		return (this.#y);
+	}
+	get orientation ()
+	{
+		return (this.#orientation);
+	}
+	set x (x)
+	{
+		this.#x = x;
+	}
+	set y (y)
+	{
+		this.#y = y;
+	}
+	set orientation (orientation)
+	{
+		this.#orientation = orientation;
+	}
+}
